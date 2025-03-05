832ff61b1cfece417cc3ac7eee9d34a541d9a62a
Emami-114 <emami.114@outlook.de>
2025-03-05 16:53:59 +0100

    added image uplaod and get

--- MalDealsBackend/Migrations/20250305105230_InitialCreate.Designer.cs
+++ MalDealsBackend/Migrations/20250305105230_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MalDealsBackend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250302173620_InitialCreate")]
+    [Migration("20250305105230_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
