a649a13fc90ee980f00d3b3e9734c87878e6338a
Emami-114 <emami.114@outlook.de>
2025-04-03 22:30:11 +0200

    adapted api_key

--- MalDealsBackend/Middleware/ApiKeyMiddleware.cs
+++ MalDealsBackend/Middleware/ApiKeyMiddleware.cs
@@ -13,7 +13,7 @@ namespace MalDealsBackend.Middleware
             var path = (context.Request.Path.Value ?? "").ToLower();
             var apiKey = context.Request.Headers["X-API-Key"]; // API-Key auch aus dem Header holen
 
-            if (path.StartsWith("/scalar") || path.StartsWith("/swagger") || path.StartsWith("/openapi") || path.StartsWith("/api/request-api-key"))
+            if (path.StartsWith("/scalar") || path.StartsWith("/swagger") || path.StartsWith("/openapi"))
             {
                 await _next(context);
                 return;
